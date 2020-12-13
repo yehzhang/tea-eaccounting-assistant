596f43690f3cf5732f440448d6f6ed597858b9a5
yehzhang <simonzhangintp@gmail.com>
2020-12-13 02:18:21 -0700

    Use axios uniformly.

--- teaDispenser/data/market/queryMarketBuyOrders.ts
+++ teaDispenser/data/market/queryMarketBuyOrders.ts
@@ -1,15 +1,17 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 import MarketQuery from './MarketQuery';
 
 async function queryMarketBuyOrders(itemTypeId: number): Promise<MarketQuery | null> {
-  const response = await fetch(`http://138.68.255.93:8000/${itemTypeId}`);
-  const result = await response.json();
-  if (typeof result !== 'object' || result === null || 'error' in result) {
-    console.error('Unexpected error in database fetch', result);
+  const response = await axios({
+    url: `http://138.68.255.93:8000/${itemTypeId}`,
+  });
+  const { data } = response;
+  if (typeof data !== 'object' || data === null || 'error' in data) {
+    console.error('Unexpected error in database fetch', data);
     return null;
   }
 
-  const fetchedAtString = (result as any).fetched_at;
+  const fetchedAtString = (data as any).fetched_at;
   const fetchedAt = new Date(fetchedAtString);
   if (isNaN(fetchedAt.getTime())) {
     console.error('Expected valid `fetched_at` from database, got', fetchedAtString);
@@ -17,7 +19,7 @@ async function queryMarketBuyOrders(itemTypeId: number): Promise<MarketQuery | n
   }
 
   return {
-    orders: (result as any).orders
+    orders: (data as any).orders
         .map((order: any) => {
           const {
             price,

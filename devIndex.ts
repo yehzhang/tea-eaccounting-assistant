548af2aa8a0327a7c6d38eefe6415f04aa76722d
Simon Zhang <simonzhangintp@gmail.com>
2021-05-21 14:36:11 -0700

    Add dev auto restart.

---
+++ devIndex.ts
@@ -0,0 +1,19 @@
+import { ChildProcess, spawn } from 'child_process';
+import chokidar from 'chokidar';
+
+const watcher = chokidar.watch('./teaDispenser');
+await new Promise((resolve) => void watcher.on('ready', resolve));
+
+function run(): ChildProcess {
+  return spawn('yarn', ['start'], {
+    stdio: 'inherit',
+  });
+}
+
+let childProcess: ChildProcess = run();
+
+watcher.on('all', () => {
+  console.log('Restarting...');
+  childProcess.kill();
+  childProcess = run();
+});
